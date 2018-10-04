594832cc3a73a296bd59e79fb8ecac85a9fded6a
Nayeem Mahmood <com.debugster@gmail.com>
2018-10-04 23:24:57 +0600

    Create Contest.cpp

---
+++ Contest.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* something starts */
+/* something ends */
+
+/* typedef starts */
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/* typedef ends */
+
+/* macro starts */
+
+#define PI acos(-1.0)
+
+/* macro ends */
+
+
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+
+    return 0;
+}
